f76cdaae185c57b69e0897fd8973a5d3d6d06cb4
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-02-12 13:10:06 +0700

    initial commit

---
+++ LandingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandingApp
+//
+//  Created by Andie Hariyadi Supriyatna on 12/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandingApp/LandingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandingAppApp.swift
+//  LandingApp
+//
+//  Created by Andie Hariyadi Supriyatna on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LandingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
