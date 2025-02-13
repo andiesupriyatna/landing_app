c58dcc383d0b84ea6174f331e7fb9b3f9ff28b7e
Andie Hariyadi Supriyatna <hariyadi.andie@gmail.com>
2025-02-13 13:03:19 +0700

    add button and label text on bottom

--- LandingApp/ContentView.swift
+++ LandingApp/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var showBars = false
+       
+       let barData = [
+           BarData(label: "現在", height: 0.25),
+           BarData(label: "3ヶ月", height: 0.4),
+           BarData(label: "1年", height: 0.7),
+           BarData(label: "2年", height: 1.0)
+       ]
+       
+       var body: some View {
+           VStack(spacing: 20) {
+               HStack {
+               Spacer()
+                   Image(systemName: "xmark")
+                       .foregroundColor(.black)
+                       .padding()
+                       .background(Color.white)
+                       .clipShape(Circle())
+                       .shadow(radius: 2)
+                   .padding()
+               }
+               
+               VStack(spacing: 8) {
+                   Text("Hello")
+                       .font(.largeTitle)
+                       .fontWeight(.bold)
+                   Text("SpeakBUDDY")
+                       .font(.largeTitle)
+                       .fontWeight(.bold)
+                   Image("speakbuddy").resizable()
+                       .frame(width: 100, height: 100)
+               }
+           }
+       }
+   }
 
-#Preview {
-    ContentView()
-}
+   struct BarData: Identifiable {
+       let id = UUID()
+       let label: String
+       let height: Double
+   }
+
+   struct ContentView_Previews: PreviewProvider {
+       static var previews: some View {
+           ContentView()
+       }
+   }
